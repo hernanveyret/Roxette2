deb83e8cfa5a4b4939a8468e354add7fd96c0ad3
hernanveyret <109307663+hernanveyret@users.noreply.github.com>
2022-07-27 00:38:37 -0300

    Add files via upload

---
+++ formulario.php
@@ -0,0 +1,16 @@
+<?PHP
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$msj = $_POST['msj'];
+$cuerpo = <<<MAIL
+Haz recibido un correo de $nombre <br />
+Su correo electronico es: $correo <br />
+Ha dejado el siguiente mensaje: <br />
+$msj
+MAIL;
+$informacion_adicional = 'MIME-Version: 1.0'. "\r\n";
+$informacion_adicional .= 'content-type: text/html; charset=utf8'. "\r\n";
+$informacion_adicional .= 'from:'.$nombre.'<'.$correo.'>'. "\r\n";
+header("location:gracias.html");
+mail('hernanveyret@hotmail.com','Mensaje dede la pagina de Roxette',$correo, $informacion_adicional);
+?>
